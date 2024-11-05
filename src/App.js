266abc50626d283f57e9162144598b388b45ded2
unknown <gt12930@gmail.com>
2024-11-05 16:42:02 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import {Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { FilePdfOutlined } from "@ant-design/icons";
 
 function App() {
+  const [cotizaciones, setCotizaciones] = useState([]);
+
+  useEffect(() => {
+
+    getCotizaciones();
+  }, []);
+  const getCotizaciones = async () => {
+    const response = await fetch(
+      `${process.env.REACT_APP_BASE}/cotizaciones/completada?tipo=S`,
+      {
+        method: "GET",
+        credentials: "include",
+      }
+    );
+    const info = await response.json();
+
+    if (info) {
+      setCotizaciones(info);
+    }
+  };
+
+  const columns = [
+    {
+      title: "COD",
+      dataIndex: "secSolMod",
+      align: "center",
+    },
+    {
+      title: "SBN",
+      dataIndex: "sbn",
+      align: "center",
+    },
+    {
+      title: "GLOSA",
+      dataIndex: "glosa",
+      align: "center",
+    },
+    {
+      title: "SERVICIO",
+      dataIndex: "nombreItem",
+      align: "center",
+    },
+    {
+      title: "DEPENDENCIA",
+      dataIndex: "nombreDependencia",
+      align: "center",
+    },
+    {
+      title: "PDF",
+      render: (_, record) => (
+        <div>
+        <FilePdfOutlined
+          style={{ color: record.pdf ? "red" : "grey", cursor: "pointer" }}
+          onClick={() => {
+            if (record.pdf) {
+              window.open(record.pdf, "_blank");
+            } else {
+              alert("No hay PDF disponible para este registro.");
+            }
+          }}
+        />
+      </div>
+      ),
+      key: "action",
+      align: "center",
+    },
+
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginTop: "20px", padding:"35px" }}>
+      <Table
+        columns={columns}
+        dataSource={cotizaciones?.map((item, index) => ({
+          ...item,
+          key: item.id || index,
+        }))}
+      />
     </div>
   );
 }
